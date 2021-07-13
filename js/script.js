b60192795fcacde3daa7cdb9f39e7347e6a91c02
Eliana-Abate <eliana.abate@outlook.it>
2021-07-13 16:45:01 +0200

    edited function cpuNumber - added conditions for pushing elements in the array 'bombs'

--- js/script.js
+++ js/script.js
@@ -8,33 +8,34 @@ Quando la partita termina, comunichiamo all'utente il suo punteggio. */
 
 //TODO 1. Creo array vuoto in cui inserire i 16 numeri generati dal computer
 //TODO 2. Creo funzione per far generare i numeri random al computer
-// TODO 3. Pongo le condizioni affinché i numeri inseriti nell'array rispettino le seguenti regole: devono essere 16 al max e diversi tra loro.
+//TODO 3. Pongo le condizioni affinché i numeri inseriti nell'array rispettino le seguenti regole: devono essere 16 al max e diversi tra loro.
 
 
 //! 1. 
 var bombs = [];
-console.log('array bombs ' + bombs)
-
-var randomBombs = cpuRandom();
-console.log('randomBombs ' + randomBombs);
-
-while (randomBombs !== randomBombs && bombs.length < 16) {
-    bombs.push(randomBombs);
-}
-
-
-
 
 
 //! 2. 
-function cpuRandom(randomNumber){
-    randomNumber = Math.floor(Math.random() * 100) + 1;
+function cpuRandom(){
+    var randomNum = Math.floor(Math.random() * 100) + 1;
 
-    for (var i = 0; i < 16; i++) {
-        randomNumber = Math.floor(Math.random() * 100) + 1;
-        console.log(randomNumber);
-    }
-
-    return randomNumber
+    return randomNum;
 }
+
+
+//! 3.
+while (bombs.length < 16) {
+    var randomNumber = cpuRandom();
+
+    if (!bombs.includes(randomNumber)){
+        bombs.push(randomNumber); 
+    } 
+}
+
+console.log('array bombs: ' + bombs);
+
+
+
+
+
 
